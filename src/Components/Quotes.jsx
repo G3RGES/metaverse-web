f1a174ff65d17fcb92ead988603bb9e222d58720
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-25 16:29:16 +0200

    Update Quotes.jsx

--- src/Components/Quotes.jsx
+++ src/Components/Quotes.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
 const Quotes = () => {
-  return <div>Quotes</div>;
+  return (
+    <>
+      <div className="container py-14 px-4 font-parisienne">
+        <h1
+          data-aos="zoom-in"
+          className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center "
+        >
+          "Virtual reality is a medium, a means by which humans can share ideas
+          and experiences."
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default Quotes;
