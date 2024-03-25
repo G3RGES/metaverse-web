546cd4a0edab7354fa937b8d66afba0cf9dba65b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-25 16:06:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import React from "react";
 
 //components
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import Quotes from "./Components/Quotes";
 
 const App = () => {
   return (
-    <main className="overflow-x-hidden">
+    <main className="overflow-x-hidden bg-white dark:bg-black">
       <Navbar />
+      <Hero />
+      <Quotes />
     </main>
   );
 };
