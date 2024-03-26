5c494087d9dddf9550072176f9bfe8833f96de2d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-26 17:11:55 +0200

    Update AppStore.jsx

--- src/Components/AppStore.jsx
+++ src/Components/AppStore.jsx
@@ -8,7 +8,33 @@ import PlayStoreImg from "../assets/website/play_store.png";
 const AppStore = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container py-10 sm:min-h-[400px] sm:grid sm:place-items-center ">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl text-center sm:text-4xl font-semibold">
+            Get Started With Our App
+          </h1>
+          <p className="text-center sm:px-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            magnam harum accusantium odit?
+          </p>
+          <div className="flex flex-wrap justify-center items-center gap-4">
+            <a href="#">
+              <img
+                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px] "
+                src={PlayStoreImg}
+                alt="playstore_image"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px] "
+                src={AppStoreImg}
+                alt="appstore_image"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
