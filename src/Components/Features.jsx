e279586f53a65c7f43de73de3e93874886664d47
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-26 15:53:49 +0200

    Update Features.jsx

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -39,7 +39,39 @@ const FeaturesData = [
 const Features = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container py-14 sm:min-h-[600px]  ">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center sm:text-4xl mb-12">
+            Why Choose Us
+          </h1>
+
+          {/* cards section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            {FeaturesData.map(
+              ({ icon, name, link, description, aosDelay }, idx) => (
+                <div
+                  className="text-center group space-y-3 sm:space-y-6 p-4 sm:py-10 bg-dark
+                  hover:bg-gradient-to-r from-primary to-secondary
+                  hover:shadow-[0_0_40px_#007cfff0] text-white hover:text-black
+                  rounded-lg duration-300"
+                  key={idx}
+                >
+                  <div className="grid place-items-center">{icon}</div>
+                  <h1 className="text-2xl">{name}</h1>
+                  <p className="">{description}</p>
+                  <a
+                    href={link}
+                    className="inline-block text-lg font-semibold py-3 text-primary 
+                            group-hover:text-black duration-300"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
