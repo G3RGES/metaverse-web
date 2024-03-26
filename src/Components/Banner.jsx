d4dc5bbc5209c48df83eb49841616bd7fed9d050
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-26 13:25:58 +0200

    Update Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// assets
+import BannerPng from "../assets/banner1.png";
+
+// icons
+import { BiPlayCircle } from "react-icons/bi";
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="py-12 sm:py-0 relative">
+      <div className="container">
+        <div className="grid ">
+          {/* image section */}
+          {/* text section */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
