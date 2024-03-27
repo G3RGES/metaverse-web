d037d7ca4a0273be2b27db29f5d75175f5ba9010
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-27 18:17:28 +0200

    Update AppStore.jsx

--- src/Components/AppStore.jsx
+++ src/Components/AppStore.jsx
@@ -10,22 +10,29 @@ const AppStore = () => {
     <>
       <div className="container py-10 sm:min-h-[400px] sm:grid sm:place-items-center ">
         <div className="space-y-6 max-w-xl mx-auto">
-          <h1 className="text-2xl text-center sm:text-4xl font-semibold">
+          <h1
+            data-aos="zoom-in"
+            className="text-2xl text-center sm:text-4xl font-semibold"
+          >
             Get Started With Our App
           </h1>
-          <p className="text-center sm:px-20">
+          <p
+            data-aos="fade-up"
+            data-aos-delay="300"
+            className="text-center sm:px-20"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
             magnam harum accusantium odit?
           </p>
           <div className="flex flex-wrap justify-center items-center gap-4">
-            <a href="#">
+            <a data-aos="fade-right" data-aos-delay="500" href="#">
               <img
                 className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px] "
                 src={PlayStoreImg}
                 alt="playstore_image"
               />
             </a>
-            <a href="#">
+            <a data-aos="fade-left" data-aos-delay="500" href="#">
               <img
                 className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px] "
                 src={AppStoreImg}
