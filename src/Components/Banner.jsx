d2b45e793fd7b554caf4c788052cca852edd051b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-27 18:17:30 +0200

    Update Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = ({ togglePlay }) => {
       <div className="container min-h-[620px] flex items-center ">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 place-items-center ">
           {/* image section */}
-          <div>
+          <div data-aos="fade-right" data-aos-once="false">
             <img
               className=" w-full max-w-[400px] "
               src={BannerPng}
@@ -23,7 +23,11 @@ const Banner = ({ togglePlay }) => {
           {/* text section */}
           <div className="lg:pr-20 relative">
             <div className="relative z-10 space-y-5">
-              <h1 className="text-4xl font-semibold ">
+              <h1
+                data-aos="fade-left"
+                data-aos-delay="300"
+                className="text-4xl font-semibold "
+              >
                 GET READY TO ENJ0Y VR{" "}
                 <span
                   className="bg-clip-text text-transparent bg-gradient-to-l from-primary
@@ -40,8 +44,16 @@ const Banner = ({ togglePlay }) => {
               </p>
 
               <div className="flex gap-6">
-                <button className="primary-btn">Get Started</button>
                 <button
+                  data-aos="fade-up"
+                  data-aos-delay="700"
+                  className="primary-btn"
+                >
+                  Get Started
+                </button>
+                <button
+                  data-aos="fade-up"
+                  data-aos-delay="900"
                   onClick={togglePlay}
                   className="flex items-center gap-2"
                 >
