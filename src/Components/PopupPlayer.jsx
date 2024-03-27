d22f6e96ebd842772f6cf6bb61be27f82e3f1911
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-27 18:05:32 +0200

    Update PopupPlayer.jsx

--- src/Components/PopupPlayer.jsx
+++ src/Components/PopupPlayer.jsx
@@ -12,11 +12,34 @@ const PopupPlayer = ({ isPlaying, togglePlay }) => {
           className="fixed z-[9999] top-0 left-0 w-full h-full
          bg-black/70 backdrop-blur-sm "
         >
-          <div className="">
-            <div className="">
+          <div
+            className="absolute w-full sm:max-w-[600px] top-1/2 left-1/2
+          -translate-x-1/2 -translate-y-1/2 "
+          >
+            <div className="bg-white dark:bg-dark p-5 m-3 rounded-lg shadow-md ">
               {/* header section */}
+              <div className="flex items-center justify-between pb-3">
+                <h1 className="text-2xl font-bold">
+                  META<span className="text-primary font-semibold">verse</span>
+                </h1>
+                <IoClose
+                  className="text-4xl font-semibold cursor-pointer hover:scale-110 duration-300"
+                  onClick={togglePlay}
+                />
+              </div>
+
+              {/* video player */}
               <div className="">
-                <h1 className="">Metaverse</h1>
+                <iframe
+                  width="100%"
+                  height="315"
+                  src="https://www.youtube.com/embed/qo86o0FllcY?si=IkhA0VsY2qnXMrHQ"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
               </div>
             </div>
           </div>
