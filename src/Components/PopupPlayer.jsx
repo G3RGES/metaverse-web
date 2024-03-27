5d8b2142bf8d6a9af4fc518413d3061a0357c616
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-03-27 15:23:50 +0200

    Update PopupPlayer.jsx

--- src/Components/PopupPlayer.jsx
+++ src/Components/PopupPlayer.jsx
@@ -1,8 +1,29 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-const PopupPlayer = () => {
-  return <div>PopupPlayer</div>;
+// icons
+import { IoClose } from "react-icons/io5";
+
+const PopupPlayer = ({ isPlaying, togglePlay }) => {
+  return (
+    <>
+      {isPlaying && (
+        <div
+          className="fixed z-[9999] top-0 left-0 w-full h-full
+         bg-black/70 backdrop-blur-sm "
+        >
+          <div className="">
+            <div className="">
+              {/* header section */}
+              <div className="">
+                <h1 className="">Metaverse</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default PopupPlayer;
